4ede008946ee291f0c249d648412f7b08078ed51
Ardiyanto Putra <77187673+ardiyantop8@users.noreply.github.com>
2023-11-07 11:00:16 +0700

    Update rooc-draggable.js

--- widget_chatbot/js/rooc-draggable.js
+++ widget_chatbot/js/rooc-draggable.js
@@ -1,38 +1,44 @@
-var draggable = document.getElementById('rooc-widget-launcher');
-var posX = 0,
-    posY = 0,
-    mouseX = 0,
-    mouseY = 0;
-var isDrag = false;
+const draggableImage = document.getElementById("draggable-image");
+let offsetX, offsetY;
+let isDragging = false;
 
-draggable.addEventListener('mousedown', mouseDown, false);
-window.addEventListener('mouseup', mouseUp, false);
-draggable.addEventListener("touchstart", touchStart, true);
-draggable.addEventListener("touchend", touchEnd, true);
-window.addEventListener("touchmove", touchMove, true);
-draggable.addEventListener("touchmove", touchMove, true);
+draggableImage.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    offsetX = e.clientX - draggableImage.getBoundingClientRect().left;
+    offsetY = e.clientY - draggableImage.getBoundingClientRect().top;
+});
 
-function mouseDown(e) {
-    e.preventDefault();
-    posX = e.clientX - draggable.offsetLeft;
-    posY = e.clientY - draggable.offsetTop;
-    window.addEventListener('mousemove', moveElement, false);
-}
-
-function mouseUp(e) {
-    window.removeEventListener('mousemove', moveElement, false);
-    if (isDrag) {
-        isDrag = false;
-    } else if (!isDrag && draggable.contains(e.target)) {
-        document.getElementById('rooc-widget-appx').style.display = "block";
-        draggable.style.display = 'none';
+document.addEventListener("mousemove", (e) => {
+    if (isDragging) {
+        const left = e.clientX - offsetX;
+        const top = e.clientY - offsetY;
+        draggableImage.style.transform = `translate(${left}px, ${top}px)`;
     }
-}
+});
 
-function moveElement(e) {
-    mouseX = e.clientX - posX;
-    mouseY = e.clientY - posY;
-    draggable.style.left = mouseX + 'px';
-    draggable.style.top = mouseY + 'px';
-    isDrag = true;
-}
+document.addEventListener("mouseup", () => {
+    isDragging = false;
+    draggableImage.style.transform = "translate(0, 0)";
+});
+
+// Event untuk perangkat sentuh (touch) seperti smartphone dan tablet
+draggableImage.addEventListener("touchstart", (e) => {
+    isDragging = true;
+    const touch = e.touches[0];
+    offsetX = touch.clientX - draggableImage.getBoundingClientRect().left;
+    offsetY = touch.clientY - draggableImage.getBoundingClientRect().top;
+});
+
+document.addEventListener("touchmove", (e) => {
+    if (isDragging) {
+        const touch = e.touches[0];
+        const left = touch.clientX - offsetX;
+        const top = touch.clientY - offsetY;
+        draggableImage.style.transform = `translate(${left}px, ${top}px)`;
+    }
+});
+
+document.addEventListener("touchend", () => {
+    isDragging = false;
+    draggableImage.style.transform = "translate(0, 0)";
+});
